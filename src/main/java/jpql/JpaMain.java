06dc20f56bd3a586115a1b4eb9b73cfa21a91577
WorkManNamChul <knc219@naver.com>
2021-05-26 17:53:14 +0900

    fetch join 진행중

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,34 +14,58 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Team team = new Team();
-            team.setName("A");
-            em.persist(team);
+            Team team1 = new Team();
+            team1.setName("팀A");
+            em.persist(team1);
+
+            Team team2 = new Team();
+            team2.setName("팀B");
+            em.persist(team2);
+
+            Team team3 = new Team();
+            team3.setName("팀C");
+            em.persist(team3);
+
 
             Member member = new Member();
-            member.setUsername("member");
+            member.setUsername("회원1");
             member.setAge(10);
-            member.changeTeam(team);
+            member.changeTeam(team1);
             member.setType(MemberType.ADMIN);
             em.persist(member);
 
             Member member2 = new Member();
-            member2.setUsername("관리자");
+            member2.setUsername("회원2");
             member2.setAge(28);
-            member2.changeTeam(team);
+            member2.changeTeam(team1);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setAge(30);
+            member3.changeTeam(team2);
+            em.persist(member3);
+
+            Member member4 = new Member();
+            member4.setUsername("회원4");
+            member4.setAge(21);
+            em.persist(member4);
 
             em.flush();
             em.clear();
-            // 경로 표현시 .(점)을 찍어 그래프를 탐색하는 것
-            // 상태필드는 더 이상 경로탐색이 안됨 상태필드 (m.username)
-            // 묵시적 조인은 사용하지 말고 명시적 조인만 사용할 것
-            // String query = "select m.team From Member m";
-            // String query = "select t.members.size From Team t";
-            String query = "select m.username From Team t join t.members m,";
-            List<String> resultList = em.createQuery(query, String.class)
+            // 페치 조인(fetch join) 실무에서 중요
+            // sql 종류가 아니고 jpql 성능최적화를 위해 제공
+            // sql을 한 번에 조회 가능
+            // join fetch 명령어 사용
+            // 즉시로딩을 명시적으로 표현하는것과 비슷함
+            String query = "select m from Member m";
+            List<Member> resultList = em.createQuery(query, Member.class)
                     .getResultList();
-            System.out.println("result = " + resultList);
+
+            for (Member member1 : resultList) {
+                System.out.println("member1.getUsername() = " + member1.getUsername());
+                System.out.println("member1.getTeam().getName() = " + member1.getTeam().getName());
+            }
 
             tx.commit();
         }catch (Exception e){
