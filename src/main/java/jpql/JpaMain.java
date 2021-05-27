0dde3b3ca221444749b0fd8410d8421db89912c4
WorkManNamChul <knc219@naver.com>
2021-05-27 11:14:50 +0900

    20210527 회사 페치조인 2 진행중

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -53,21 +53,16 @@ public class JpaMain {
 
             em.flush();
             em.clear();
-            // 페치 조인(fetch join) 실무에서 중요
-            // sql 종류가 아니고 jpql 성능최적화를 위해 제공
-            // sql을 한 번에 조회 가능
-            // join fetch 명령어 사용
-            // 즉시로딩을 명시적으로 표현하는것과 비슷함
-            // 지연로딩으로 새로운 데이터를 가져올때마다 쿼리를 날리기 때문에 쿼리가 너무 많이 사용됨(fetch 사용x 일때)
-            // fetch : 조인해서 필요한 데이터를 한 번에 모두 가져온다.
-            // String query = "select m from Member m";
-            String query = "select m from Member m join fetch m.team t";
-            List<Member> resultList = em.createQuery(query, Member.class)
+
+            String query = "select t from Team t";
+            List<Team> resultList = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
-            for (Member member1 : resultList) {
-                System.out.println("member1.getUsername() = " + member1.getUsername());
-                System.out.println("member1.getTeam().getName() = " + member1.getTeam().getName());
+            for (Team team : resultList) {
+                System.out.println("team.getName() = " + team.getName());
+                System.out.println("team.getMembers() = " + team.getMembers().size());
             }
 
             tx.commit();
